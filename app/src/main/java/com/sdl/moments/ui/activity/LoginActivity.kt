907e4b4408faed23323d25611406cc2a404f5a4e
songdongliang <sdl_itperson@outlook.com>
2018-06-05 20:25:09 +0800

    Initial commit

---
+++ app/src/main/java/com/sdl/moments/ui/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.sdl.moments.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sdl.moments.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
